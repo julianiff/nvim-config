f63acace8a946557bc8269fc674e51293f693a14
julianiff <julian.iff@frontify.com>
2024-10-06 08:42:59 +0200

    chore: use format on save for js

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -12,10 +12,38 @@ return {
 		lazy = false,
 		config = function()
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
 			local lspconfig = require("lspconfig")
+
+			local function organize_imports()
+				local params = {
+					command = "_typescript.organizeImports",
+					arguments = { vim.api.nvim_buf_get_name(0) },
+					title = "",
+				}
+				vim.lsp.buf.execute_command(params)
+			end
 			lspconfig.ts_ls.setup({
 				capabilities = capabilities,
+				commands = {
+					OrganizeImports = {
+						organize_imports,
+						description = "Organize Imports",
+					},
+				},
+				on_attach = function(client, bufnr)
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						buffer = bufnr,
+						command = "OrganizeImports",
+					})
+				end,
+			})
+			lspconfig.eslint.setup({
+				on_attach = function(client, bufnr)
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						buffer = bufnr,
+						command = "EslintFixAll",
+					})
+				end,
 			})
 			lspconfig.solargraph.setup({
 				capabilities = capabilities,
