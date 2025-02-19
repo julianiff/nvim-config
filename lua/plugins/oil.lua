23225f206a2860a30d4b535792ab09b6ecb50a0e
julianiff <julian.iff@frontify.com>
2025-02-19 22:10:46 +0100

    chore: add somechore: add cute icons and readd telescope

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,10 +1,14 @@
 return {
 	"stevearc/oil.nvim",
+	dependencies = { { "echasnovski/mini.icons", opts = {} } },
 	config = function()
 		local oil = require("oil")
 		oil.setup({
 			view_options = {
 				show_hidden = true,
+			},
+			keymaps = {
+				["<C-p>"] = false,
 			},
 		})
 		vim.keymap.set("n", "-", oil.open, {})
