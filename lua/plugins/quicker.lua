02358feaea101891ad5f0a7809cf32942f037d3a
julianiff <julian.iff@frontify.com>
2025-04-02 10:03:33 +0200

    chore: add flash and quicker

--- lua/plugins/quicker.lua
+++ lua/plugins/quicker.lua
@@ -4,4 +4,12 @@ return {
 	---@module "quicker"
 	---@type
 	opts = {},
+	config = function()
+		local quicker = require("quicker")
+		quicker.setup({
+			max_filename_width = function()
+				return math.floor(math.min(45, vim.o.columns / 2))
+			end,
+		})
+	end,
 }
