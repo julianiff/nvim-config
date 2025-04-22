74921732d9f4425ae183621788a29086ab53b810
julianiff <julian.iff@frontify.com>
2025-04-22 08:29:18 +0200

    chore: adjust lualine and flash

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -9,7 +9,43 @@ return {
 			theme = "rosebones",
 		},
 		sections = {
-			lualine_c = { { "filename", path = 1 } },
+			lualine_c = {
+				{
+					"filename",
+					path = 1,
+					fmt = function(str)
+						local parts = {}
+						for part in string.gmatch(str, "[^/]+") do
+							table.insert(parts, part)
+						end
+
+						if #parts <= 2 then
+							return str
+						end
+
+						local last_three = {
+							parts[#parts - 1],
+							parts[#parts],
+						}
+
+						local shortened_parts = {}
+						for i = 1, #parts - 2 do
+							-- Get first letter of the part
+							local first_letter = string.sub(parts[i], 1, 1)
+							table.insert(shortened_parts, first_letter)
+						end
+
+						local abbreviated_prefix = table.concat(shortened_parts, "/")
+						local full_suffix = table.concat(last_three, "/")
+
+						if abbreviated_prefix ~= "" then
+							return abbreviated_prefix .. "/" .. full_suffix
+						else
+							return full_suffix
+						end
+					end,
+				},
+			},
 			lualine_x = {
 				{
 					function()
