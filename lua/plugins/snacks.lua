5c522e56459ab109be94bd0f037a201f7dfa3e4b
julianiff <julian.iff@frontify.com>
2025-09-12 21:51:14 +0200

    chore: new start screen

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -7,10 +7,18 @@ return {
 		bigfile = { enabled = true },
 		dashboard = {
 			enabled = true,
+			sections = {
+				{ section = "header" },
+				{ section = "keys", gap = 1, padding = 1 },
+				{ pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+				{ pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+			},
 		},
-		notifier = {
+		lazygit = {
+			configure = false,
+		},
+		quickfile = {
 			enabled = true,
-			timeout = 3000,
 		},
 	},
 	keys = {
