c96d9e23f35516e3573ff4fd74d4325ce09738dc
julianiff <julian.iff@frontify.com>
2025-09-12 21:00:04 +0200

    chore: fix diagnostic

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -108,8 +108,7 @@ vim.api.nvim_set_keymap("i", "jk", "<Esc>", { noremap = true, silent = true })
 
 -- Diagnostic keymaps
 vim.keymap.set("n", "<leader>dq", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
-vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to prev [D]iagnostic message" })
-vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
+vim.keymap.set("n", "<leader>d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
